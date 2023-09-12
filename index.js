7a8c074f6faea2738f8ecdcd544149e88cbb2f12
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2023-09-12 17:12:13 +0530

    Updated js

--- index.js
+++ index.js
@@ -83,3 +83,12 @@ function isValidEmail(email) {
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailPattern.test(email);
 }
+
+function getCurrentDate() {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const day = String(now.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+}
+document.getElementById("dob").setAttribute("max", getCurrentDate());
