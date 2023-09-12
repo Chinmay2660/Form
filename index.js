afdd707e3bac5a7478d17503c43de59b4bb26ae8
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2023-09-12 16:45:59 +0530

    Add alerts for required field

--- index.js
+++ index.js
@@ -1,31 +1,85 @@
-function addToTable() {
-  var firstName = document.getElementById("fname").value;
-  var lastName = document.getElementById("lname").value;
-  var email = document.getElementById("email").value;
-  var dob = document.getElementById("dob").value;
-  var address = document.getElementById("address").value;
+function initializeTable() {
+  const table = document.querySelector("#data");
+  const thead = table.createTHead();
+  const row = thead.insertRow();
+  const cell1 = row.insertCell(0);
+  const cell2 = row.insertCell(1);
+  const cell3 = row.insertCell(2);
+}
 
-  var table = document.querySelector("#data");
+window.onload = initializeTable;
 
-  var row = table.insertRow();
+document.querySelector("button").addEventListener("click", function (event) {
+  event.preventDefault();
 
-  var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-  var cell3 = row.insertCell(2);
-  var cell4 = row.insertCell(3);
-  var cell5 = row.insertCell(4);
+  if (!validateForm()) {
+    return;
+  }
 
-  cell1.innerHTML = firstName;
-  cell2.innerHTML = lastName;
-  cell3.innerHTML = email;
-  cell4.innerHTML = dob;
-  cell5.innerHTML = address;
+  const firstName = document.getElementById("fname").value;
+  const lastName = document.getElementById("lname").value;
+  const email = document.getElementById("email").value;
+  const dob = document.getElementById("dob").value;
+  const address = document.getElementById("address").value;
+
+  const tableBody = document.getElementById("table-body");
+
+  const data = [
+    { key: "First Name", value: firstName },
+    { key: "Last Name", value: lastName },
+    { key: "Email", value: email },
+    { key: "Date of Birth", value: dob },
+    { key: "Address", value: address },
+  ];
+
+  data.forEach(function (item, index) {
+    const row = tableBody.insertRow();
+
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    const cell3 = row.insertCell(2);
+
+    cell1.innerHTML = index + 1;
+    cell2.innerHTML = item.key;
+    cell3.innerHTML = item.value;
+  });
 
   document.getElementById("fname").value = "";
   document.getElementById("lname").value = "";
   document.getElementById("email").value = "";
   document.getElementById("dob").value = "";
   document.getElementById("address").value = "";
+
+  document.getElementById("table-container").style.display = "block";
+});
+
+function validateForm() {
+  const firstName = document.getElementById("fname").value;
+  const lastName = document.getElementById("lname").value;
+  const email = document.getElementById("email").value;
+
+  if (firstName.trim() === "") {
+    alert("Please enter your First Name");
+    return false;
+  }
+
+  if (lastName.trim() === "") {
+    alert("Please enter your Last Name");
+    return false;
+  }
+
+  if (email.trim() === "") {
+    alert("Please enter your Email");
+    return false;
+  } else if (!isValidEmail(email)) {
+    alert("Please enter a valid email address");
+    return false;
+  }
+
+  return true;
 }
 
-document.querySelector("button").addEventListener("click", addToTable);
+function isValidEmail(email) {
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailPattern.test(email);
+}
